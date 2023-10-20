0137d5c3ff2e482a596ec0a5b5f13e9166eebd14
Darestar1 <karasuyasin06@gmail.com>
2023-10-20 14:43:10 +0300

    paginator and searchbar are done

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Cart,CartItem
 from store.models import Product
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def cart(request,total=0,quantity=0,cart_items=None):
     try:
+        tax=0
+        grand_total=0
         cart=Cart.objects.get(cart_id=_cart_id(request))
         cart_items= CartItem.objects.filter(cart=cart,is_active=True)
         for cart_item in cart_items:
@@ -12,7 +14,7 @@ def cart(request,total=0,quantity=0,cart_items=None):
             quantity +=cart_item.quantity
         tax=(2*total)/100
         grand_total = total+tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass    
         
     
